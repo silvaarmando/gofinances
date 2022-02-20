fd8db1ef6405574f4b51957ce9450b77744dda23
Armando Silva <franciscosilva7877@gmail.com>
2022-02-19 23:41:59 -0300

    Add Dashboard Initial

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {
+  Text,
+  View
+} from 'react-native'
+
+export function Dashboard() {
+  return (
+    <View>
+      <Text>
+        Dashboard
+      </Text>
+    </View>
+  )
+}
