bac13410fa2b82e57eb66a99ce6696ab3690c079
Armando Silva <franciscosilva7877@gmail.com>
2022-02-20 23:59:37 -0300

    Add global Theme Styles Colors

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import {
+  ThemeProvider
+} from 'styled-components'
 
+import theme from './src/global/styles/theme'
 import {
   Dashboard
 } from './src/pages/Dashboard'
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider
+      theme={theme}
+    >
       <Dashboard />
-    </>
+    </ThemeProvider>
   );
 }
