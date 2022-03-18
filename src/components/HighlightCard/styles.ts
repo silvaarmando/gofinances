72592aaa0d3abc160edfae5192157ea11c99b5dc
Armando Silva <franciscosilva7877@gmail.com>
2022-03-17 23:57:09 -0300

    Add Update HightLightCard

--- src/components/HighlightCard/styles.ts
+++ src/components/HighlightCard/styles.ts
@@ -9,16 +9,17 @@ import {
 } from '@expo/vector-icons'
 
 export const Container = styled.View`
-`;
-
-export const Header = styled.View`
   background-color: ${
-    ({ theme }) => theme.colors.shape
+  ({ theme }) => theme.colors.shape
   };
   width: ${RFValue(300)}px;
   border-radius: 5px;
   padding: 19px 23px;
   padding-bottom: ${RFValue(42)}px;
+`;
+
+export const Header = styled.View`
+
 `;
 
 export const Title = styled.Text`
@@ -31,6 +32,10 @@ export const Footer = styled.View`
 `;
 
 export const Amount = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.medium};
+  font-size: ${RFValue(32)}px;
+
+  color: ${({ theme }) => theme.fonts.medium}
 `;
 
 export const LastTransaction = styled.Text`
