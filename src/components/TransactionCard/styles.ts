0b37b90ba2a3d4a372f1021a1a3d6738b876671b
Armando Silva <franciscosilva7877@gmail.com>
2022-03-21 23:58:28 -0300

    Add TransactionaCard

---
+++ src/components/TransactionCard/styles.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components/native'
+
+import {
+  Feather
+} from '@expo/vector-icons'
+
+export const Container = styled.View`
+`;
+
+export const Title = styled.Text`
+`;
+
+export const Amount = styled.Text`
+`;
+
+export const Footer = styled.View`
+`;
+
+export const Category = styled.View`
+`;
+
+export const Icon = styled(Feather)`
+`;
+
+export const CategoryName = styled.Text`
+`;
+
+export const Date = styled.Text`
+`;
+function Feather(Feather: any) {
+    throw new Error('Function not implemented.');
+}
+
